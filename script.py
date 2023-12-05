5d491b1e52de7e72a843aca29d4dfad3cafaa068
VCM admin account <vcm@vcm-30918.vm.duke.edu>
2023-12-04 22:16:24 -0500

    update connect db part

--- script.py
+++ script.py
@@ -1,28 +1,36 @@
 import pandas as pd
-import MySQLdb
+import mysql.connector
 import os
 from dotenv import load_dotenv
 
 # Load environment variables
 load_dotenv()
 
+
 def get_connection():
-    return MySQLdb.connect(
+    return mysql.connector.connect(
         host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USER"),
-        passwd=os.getenv("DB_PASSWORD"),
-        db=os.getenv("DB_NAME"),
-        port=int(os.getenv("DB_PORT"))
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT")),
     )
+
 
 def save_data(connection, row):
     cursor = connection.cursor()
     MMSA, total_percent, *rest = row
-    total_percent = float(total_percent.strip('%')) / 100 if '%' in total_percent else None
-    values = [MMSA, total_percent] + [None if r == 'NA' else r for r in rest]
+    # original code
+    # total_percent = float(total_percent.strip('%'))
+    # / 100 if '%' in total_percent else None
+    total_percent = total_percent.strip("%") if "%" in total_percent else None
+    total_percent = float(total_percent) / 100 if total_percent is not None else None
+
+    values = [MMSA, total_percent] + [None if r == "NA" else r for r in rest]
     query = "INSERT INTO icu_beds VALUES (%s, %s, %s, %s, %s, %s, %s);"
     cursor.execute(query, values)
     connection.commit()
+
 
 def save_all_data():
     df = pd.read_csv("./mmsa-icu-beds.csv", encoding="utf-8")
@@ -34,6 +42,7 @@ def save_all_data():
             print(e)
             continue
     connection.close()
+
 
 if __name__ == "__main__":
     save_all_data()
